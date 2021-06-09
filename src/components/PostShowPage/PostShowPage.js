09a483fc716b77132b660f5a5e7d137ae81a6836
lucasfranchini <lucasfranchini.a@gmail.com>
2021-06-09 19:36:18 -0300

    asking for a specific post

--- src/components/PostShowPage/PostShowPage.js
+++ src/components/PostShowPage/PostShowPage.js
@@ -15,12 +15,9 @@ export default function PostShowPage() {
   const history = useHistory();
 
   useEffect(() => {
-    setPost({
-      id: 1,
-      title: 'Hello World',
-      coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-      contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
-      content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...'
+    const promise = axios.get(`http://localhost:4000/posts/${postId}`);
+    promise.then(res =>{
+      setPost(res.data);
     })
   }, [postId]);
 
